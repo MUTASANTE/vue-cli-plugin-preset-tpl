d88d80b83381af6176ae3b66ebe7e60a8b7eb555
tbl0605 <tbl0605@gmail.com>
2020-02-07 11:01:36 +0100

    update DOS launcher

--- generator/template/run-server-dev.cmd
+++ generator/template/run-server-dev.cmd
@@ -1,4 +1,5 @@
 REM Mise a jour des dependances du projet
 call "<%= globalScriptsPath.replace(/^\/|\/$/g, '') %>\npm" update
 REM Lancement du serveur de dev
-call "<%= globalScriptsPath.replace(/^\/|\/$/g, '') %>\create_watch_vuejs_project.cmd" "%~dp0" N
+set current_dir=%~dp0
+call "<%= globalScriptsPath.replace(/^\/|\/$/g, '') %>\create_watch_vuejs_project.cmd" "%current_dir:~0,-1%"
