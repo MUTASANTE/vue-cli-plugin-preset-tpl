37f8a2bec333616138506971295bc963e33be23b
tbl0605 <tbl0605@gmail.com>
2020-03-14 10:52:14 +0100

    Add some vue-router IE & Edge workarounds

--- generator/template/src/router.js
+++ generator/template/src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import { detectIE } from './services/auth.compat';
 const Home = () => import('./views/Home.vue');
 
 Vue.use(Router);
@@ -24,6 +25,28 @@ export const routes = [
   { path: '*', component: () => import('./components/ComponentNotFound') }
 ];
 
+// XXX : bug sous Edge !!! A remonter à vue-router ?
+// Voir aussi https://github.com/vuejs/vue-router/pull/2774
+if (detectIE()) {
+  let oldReplaceState = window.history.replaceState;
+  window.history.replaceState = function(...args) {
+    let matches,
+      newArgs = [...args];
+    if (
+      newArgs.length >= 3 &&
+      newArgs[2] &&
+      // Il faut supprimer le lecteur Windows de chaque Url "locale" (de type file:)
+      // sinon "window.history.replaceState(stateCopy, '', absolutePath);"
+      // dans la fonction setupScroll() de Vue-router ne fonctionnera pas !
+      (matches = /^\/[A-Z]:(\/.*)$/gi.exec(newArgs[2].toString())) &&
+      matches.length == 2
+    ) {
+      newArgs[2] = matches[1];
+    }
+    return oldReplaceState.apply(this, newArgs);
+  };
+}
+
 export default new Router({
   routes,
   // https://cli.vuejs.org/config/#publicpath
@@ -31,7 +54,6 @@ export default new Router({
     process.env
       .BASE_URL /* est (re)défini grâce à la propriété publicPath du fichier vue.config.js */,
   mode: process.env.VUE_APP_ROUTE_MODE /* 'history' or 'hash' */,
-  // XXX : bug sous Edge !!! A supprimer !!!
   scrollBehavior(to, from, savedPosition) {
     if (to.hash && process.env.VUE_APP_ROUTE_MODE !== 'hash') {
       return { selector: to.hash };
---
+++ generator/template/src/services/auth.compat.js
@@ -0,0 +1,78 @@
+function isLocalStorageAvailable() {
+  try {
+    // https://stackoverflow.com/questions/32374875/localstorage-not-working-in-edge
+    // Local Storage didn't work for local files in IE9, so I imagine that this is still the case in MS Edge.
+    localStorage.getItem('__CHECK_IF_ACCESSIBLE__');
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+// https://stackoverflow.com/questions/32374875/localstorage-not-working-in-edge
+function detectIE() {
+  if (!window || !window.navigator || !window.navigator.userAgent) {
+    return false;
+  }
+
+  var ua = window.navigator.userAgent;
+
+  var msie = ua.indexOf('MSIE ');
+  if (msie > 0) {
+    if (process.env.VUE_APP_DEBUG_MODE && console) console.log('IE detected!');
+    // IE 10 or older => return version number
+    return true;
+  }
+
+  var trident = ua.indexOf('Trident/');
+  if (trident > 0) {
+    if (process.env.VUE_APP_DEBUG_MODE && console) console.log('IE detected!');
+    // IE 11 => return version number
+    return true;
+  }
+
+  var edge = ua.indexOf('Edge/');
+  if (edge > 0) {
+    if (process.env.VUE_APP_DEBUG_MODE && console) console.log('IE detected!');
+    // Edge (IE 12+) => return version number
+    return true;
+  }
+
+  // other browser
+  return false;
+}
+
+// On ne peut pas utiliser l'objet global "localStorage" lorsqu'IE (ou Edge)
+// travaille avec des fichiers locaux accédés via le protocole "file:"
+var storageObject =
+  !isLocalStorageAvailable() && detectIE()
+    ? {
+        setItem(name, value, days = 0) {
+          var expires = '';
+          if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+            expires = '; expires=' + date.toUTCString();
+          }
+          document.cookie = name + '=' + (value || '') + expires + '; path=/';
+        },
+
+        removeItem(name) {
+          this.setItem(name, '', -1);
+        },
+        getItem(name) {
+          var nameEQ = name + '=';
+          var ca = document.cookie.split(';');
+          for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0)
+              return c.substring(nameEQ.length, c.length);
+          }
+          return null;
+        }
+      }
+    : localStorage;
+
+export { detectIE };
+export default storageObject;
