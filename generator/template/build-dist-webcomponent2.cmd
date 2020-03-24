341de3421cb184f77c2c23fd83fb3d1a88f164dd
tbl0605 <tbl0605@gmail.com>
2020-03-24 09:36:10 +0100

    Fix web component builds

--- generator/template/build-dist-webcomponent2.cmd
+++ generator/template/build-dist-webcomponent2.cmd
@@ -2,11 +2,11 @@ REM Necessite karol-f/vue-custom-element
 
 setlocal enableDelayedExpansion
 
-for /F %%i in ('findstr /B /L VUE_APP_WEB_COMPONENT_NAME= "%~dp0.env.webcomponent"') do set "%%i"
+for /F %%i in ('findstr /B /L VUE_APP_WEB_COMPONENT_NAME= "%~dp0.env.webcomponent2"') do set "%%i"
 
 if not defined VUE_APP_WEB_COMPONENT_NAME (
    echo.
-   echo La variable VUE_APP_WEB_COMPONENT_NAME n'a pu etre trouvee dans le fichier "%~dp0.env.webcomponent"
+   echo La variable VUE_APP_WEB_COMPONENT_NAME n'a pu etre trouvee dans le fichier "%~dp0.env.webcomponent2"
    set ERRORLEVEL=1
    goto fin
 )
