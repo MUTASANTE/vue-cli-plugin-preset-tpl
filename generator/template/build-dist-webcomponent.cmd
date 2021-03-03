3220aa10c34f1c19d6f1db812b58218a0b9e73b5
tbl0605 <tbl0605@gmail.com>
2021-03-03 11:09:57 +0100

    Update to latest versions

--- generator/template/build-dist-webcomponent.cmd
+++ generator/template/build-dist-webcomponent.cmd
@@ -10,3 +10,4 @@ call "<%= globalScriptsPath.replace(/^\/|\/$/g, '') %>\npm" dedupe
 call "<%= globalScriptsPath.replace(/^\/|\/$/g, '') %>\npx" vue-cli-service build --target wc --name my-custom-element ./src/WebComponent.vue --mode webcomponent
 
 call powershell -Command "(gc """"%~dp0dist\demo.html"""") -replace '<my-custom-element>', '<my-custom-element id=""""myapp"""">' | Out-File -encoding utf8 """"%~dp0dist\demo.html""""
+if exist "%~dp0dist\demo.html.gz" del /Q "%~dp0dist\demo.html.gz"
--- generator/template/build-dist-webcomponent2.cmd
+++ generator/template/build-dist-webcomponent2.cmd
@@ -19,6 +19,7 @@ call "<%= globalScriptsPath.replace(/^\/|\/$/g, '') %>\npm" prune
 call "<%= globalScriptsPath.replace(/^\/|\/$/g, '') %>\npm" dedupe
 call "<%= globalScriptsPath.replace(/^\/|\/$/g, '') %>\npm" run build -- --mode webcomponent2
 
-call powershell -Command "(gc """"%~dp0dist\index.html"""") -replace '<div id=app></div>', '<!VUE_APP_WEB_COMPONENT_NAME!></!VUE_APP_WEB_COMPONENT_NAME!>' | Out-File -encoding utf8 """"%~dp0dist\index.html""""
+call powershell -Command "(gc """"%~dp0dist\index.html"""") -replace '<div id=""""app""""></div>', '<!VUE_APP_WEB_COMPONENT_NAME!></!VUE_APP_WEB_COMPONENT_NAME!>' | Out-File -encoding utf8 """"%~dp0dist\index.html""""
+if exist "%~dp0dist\index.html.gz" del /Q "%~dp0dist\index.html.gz"
 
 :fin
