7e90fe07541f7d56e8f9da882826a89956886b70
tbl0605 <tbl0605@gmail.com>
2021-03-03 10:30:05 +0100

    Update some project configuration files

--- generator/template/_eslintrc.js
+++ generator/template/_eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
     'prettier/vue'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
